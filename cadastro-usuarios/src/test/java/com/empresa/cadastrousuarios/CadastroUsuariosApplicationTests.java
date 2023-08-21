5517bbfeb3fd89a6acefd0399c6ba54bd2e4c07a
Marcos Vinícius <vo7775832@gmail.com>
2023-08-21 18:49:15 -0300

    feat: configuracao inicial do projeto

---
+++ cadastro-usuarios/src/test/java/com/empresa/cadastrousuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.cadastrousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
